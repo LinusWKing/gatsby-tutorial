919acd60410ce048e1f1c0c3b9f173bc8f25440d
linuswking <linuswahomeking@gmail.com>
2024-06-13 15:56:10 +0300

    progressing well

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import Layout from '../components/Layout'
+const About = () => {
+    return (
+        <Layout pageTitle="About">
+            <p>
+                Now view the about me!
+            </p>
+        </Layout>
+
+    )
+}
+
+export const Head = () => <title>About page</title>
+
+export default About
